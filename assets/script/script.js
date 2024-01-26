21ff984e48668d59870ea48c66bbfe04c2069ff8
Lucas Tozo <lucastozo7@gmail.com>
2024-01-25 23:52:19 -0400

    back-to-top button added

--- assets/script/script.js
+++ assets/script/script.js
@@ -101,3 +101,26 @@ function ExtractVideoId(videoUrl){
     }
     return videoId;
 }
+
+//back to top button
+document.addEventListener('DOMContentLoaded', (event) => {
+    let backToTopButton = document.querySelector("#btn-back-to-top");
+
+    window.onscroll = function(){
+        scrollFunction();
+    };
+
+    function scrollFunction(){
+        if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+            backToTopButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+        }
+    }
+
+    backToTopButton.addEventListener("click", backToTop);
+    function backToTop(){
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+});
