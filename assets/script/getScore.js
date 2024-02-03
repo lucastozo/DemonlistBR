a3b4ba987f5fb41b481e57b91454888888dfadbc
Lucas Tozo <lucastozo7@gmail.com>
2024-02-03 02:02:33 -0400

    Score Formula Changed

--- assets/script/getScore.js
+++ assets/script/getScore.js
@@ -1,7 +1,7 @@
 // EQUAÇÂO DE PONTUAÇÂO ABAIXO, IMPORTANTE
-// latex pontuação geral: P = \sum_{i \in L} \frac{100}{\sqrt{p_i}}
+// latex pontuação geral: P = \sum_{i \in L} \frac{200}{e^{\frac{X}{20}}}
 function getScore(position) {
-    return 100 / Math.sqrt(position);
+    return 200 / Math.exp(position / 20);
 }
 function getScoreProgress(position, progress) {
     return getScore(position) * (progress / 100);
