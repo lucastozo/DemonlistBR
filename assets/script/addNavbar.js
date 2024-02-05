b58e227abd9e2a5b0641d1b91a8f1ad303e5ad6d
Lucas Tozo <lucastozo7@gmail.com>
2024-02-05 02:37:41 -0400

    fix: navbar loading speed

--- assets/script/addNavbar.js
+++ assets/script/addNavbar.js
@@ -1,10 +1,8 @@
 // navbar location: assets/components/navbar.html
-window.onload = function () {
-    fetch('/assets/components/navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            let div = document.createElement('div');
-            div.innerHTML = data;
-            document.body.insertBefore(div, document.body.firstChild);
-        });
-}
+fetch('/assets/components/navbar.html')
+    .then(response => response.text())
+    .then(data => {
+        let div = document.createElement('div');
+        div.innerHTML = data;
+        document.body.insertBefore(div, document.body.firstChild);
+    });
